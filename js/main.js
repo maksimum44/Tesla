a6673b69f70f739e4b1ab2777482253903bedc68
maksimum44 <educations.maxim@gmail.com>
2020-06-17 01:54:39 +0300

    Added menu and video trailer

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ $('.slider').slick({
     autoplay: 3000,
     dots: true,
 });
+
+$('.header-btn').on('click', function(){
+    $('.menu, .header-content, .header-top').addClass('active');
+});
+
+$('.close-btn').on('click', function(){
+    $('.menu, .header-content, .header-top').removeClass('active');
+});
+
+
 });
